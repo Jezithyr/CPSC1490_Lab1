ed5353e2cb5c49f2c1af584e6e7aa2ae5cac4b6b
xingchaohe <35545870+xingchaohe@users.noreply.github.com>
2018-01-17 15:31:45 -0800

    Add files via upload

---
+++ light.ino
@@ -0,0 +1,34 @@
+const byte pinSetup=11111100;
+byte pinstates=11111100;
+
+void pinwrite(byte pinstates){
+  PORTC=pinstates;
+}
+byte pinread(){
+  return PORTC;
+}
+   int lightmain[]={0,0,0};
+   int lightcross[]={0,0,0};
+
+void setup() {
+  // put your setup code here, to run once:
+   DDRC=pinSetup;
+   pinwrite(11111100);
+  
+}
+
+void loop() {
+   int startTime = millis();
+   int count=0;
+   while (true){
+    int index=count%4;
+    count++;
+    switch (index){
+      case 0: lightmain[0]=1; lightcross[2]=1; break;
+      case 1: lightmain[1]=1; lightcross[2]=1; break;
+      case 2: lightmain[2]=1; lightcross[0]=1; break;
+      case 3: lightmain[2]=1; lightcross[1]=1; break;
+    }
+    lightmain[]={0,0,0};
+    lightcross[]={0,0,0};
+}
