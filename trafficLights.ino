c4a5f6f58a5680761c15a6aad2291d07662d7275
Jesse Rougeau <jmaster9999@gmail.com>
2018-01-17 14:39:01 -0800

    Add files via upload

---
+++ trafficLights.ino
@@ -0,0 +1,37 @@
+const byte pinSetup = 11111100;
+byte pinstates = 11111100;
+byte delaySettings[4][3] = {{1,1,1},{2,1,2},{5,2,5},{30,10,30}};
+
+void pinwrite(byte pinstates){
+  PORTC=pinstates;
+}
+byte pinread(){
+  return PORTC;
+}
+
+
+void setup() {
+  DDRC=pinSetup;
+  pinwrite(11111100);
+}
+
+void loop() {
+int startTime = millis();
+bool cycle = true;
+
+while (cycle){
+  pinstates = pinread();
+  
+  
+  
+  
+  
+}
+
+
+
+
+
+
+
+}
